a28bb3841e8b11d20102c1e69c5c9a99fc065f1d
Lay <brownsugar.idv.tw@gmail.com>
2022-07-05 12:23:31 +0800

    chore: remove max scale option

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -64,7 +64,7 @@ useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} - ${title}` : title
   },
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  viewport: 'width=device-width, initial-scale=1',
   charset: 'utf-8',
   htmlAttrs: {
     lang: 'en'
